8bec9e0123c757fc0c77d9d6c139a2f0e0e165bc
mayabyle <mayab@moveo.com>
2024-01-15 14:25:42 +0200

    "removed environment from git"

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment: {
-  production: boolean;
-  GOOGLE_MAPS_API_KEY: string;
-} = {
-  production: false,
-  GOOGLE_MAPS_API_KEY: 'AIzaSyCk24I_i4Ls-XSGEdX3XD-ws3s9xOvpTxE',
-};
